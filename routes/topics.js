1f0a59aad5b94ae70c1ae09e4ccc07bedc9d2121
Henrik Nygren <nygrenh@gmail.com>
2015-05-10 19:40:59 +0300

    Viikko 7, tehtävä 50

--- routes/topics.js
+++ routes/topics.js
@@ -9,22 +9,28 @@ var Models = require('../models');
 // GET /topics
 router.get('/', function(req, res, next) {
     // Hae kaikki aihealueet tässä (Vinkki: findAll)
-    res.send(200);
+    Models.Topic.findAll().then(function(topics) {
+       res.json(topics);
+    });
 });
 
 // GET /topics/:id
 router.get('/:id', function(req, res, next) {
   // Hae aihealue tällä id:llä tässä (Vinkki: findOne)
   var topicId = req.params.id;
-  res.send(200);
+  Models.Topic.findOne(topicId).then(function(topic) {
+    res.json(topic);
+  });
 });
 
 // POST /topics
 router.post('/', function(req, res, next) {
   // Lisää tämä aihealue
   var topicToAdd = req.body;
+  Models.Topic.create(topicToAdd).then(function(topic) {
+    res.json(topic);
+  });
   // Palauta vastauksena lisätty aihealue
-  res.send(200);
 });
 
 // POST /topics/:id/message
@@ -33,8 +39,11 @@ router.post('/:id/message', function(req, res, next) {
   var topicId = req.params.id;
   // ...tämä viesti (Vinkki: lisää ensin messageToAdd-objektiin kenttä TopicId, jonka arvo on topicId-muuttujan arvo ja käytä sen jälkeen create-funktiota)
   var messageToAdd = req.body;
+  messageToAdd.TopicId = topicId;
   // Palauta vastauksena lisätty viesti
-  res.send(200);
+  Models.Topic.create(messageToAdd).then(function(topic) {
+    res.json(topic);
+  });
 });
 
 module.exports = router;
